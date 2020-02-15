072fbc022a2ad04e87d43f4ef54e70e3fa100a89
Kefan Yang <starordust@gmail.com>
2020-02-15 14:19:31 -0800

    reset hostname

---
+++ src/deckBuilder.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import cardInfo from './settings/card.json';
+
+class DeckBuilder extends Component {
+    static propTypes = {
+    };
+
+    state = {
+        //selected: null
+    };
+
+    render() {
+        return (
+        );
+    }
+}
+
+export default DeckBuilder;
